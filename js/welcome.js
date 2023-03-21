e18ae0733276aa5bc9e1327036c69226e192f438
Lawrence <lawrence.0725@hotmail.com>
2023-03-21 22:51:21 +0800

    Add Welcome

---
+++ js/welcome.js
@@ -0,0 +1,22 @@
+// Popup close
+function popClose(e) {
+    if (e.classList.contains('pop'))
+        window.onclick  = function(event) {
+            if (event.target == e) {
+                e.children[0].style.animationName = "popdown";
+                setTimeout(function(){
+                    e.style.display = "none";
+                    e.children[0].style.animationName = "popup";
+                }, 500); }}
+    else if (!(e.classList.contains('pop__body'))) {
+        e.closest('.pop').children[0].style.animationName = "popdown";
+        setTimeout(function(){
+            e.closest('.pop').style.display = "none";
+            e.closest('.pop').children[0].style.animationName = "popup";
+        }, 500); }
+    document.body.style.overflow = "scroll"; }
+
+// 多國語popup
+function popLangauges() {
+    document.getElementById("pop__langauges").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
