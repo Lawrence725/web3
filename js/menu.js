da3e337279a4fa0ffc80c431555e6c98977d0c9a
Lawrence <lawrence.0725@hotmail.com>
2023-03-22 23:43:57 +0800

    Add Menu cart

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+// 購物車popup
+function popCart() {
+    document.getElementById("pop__cart").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
+
+function textarea(e) {
+    e.style.height = "5px";
+    e.style.height = (e.scrollHeight)+"px";
+}
