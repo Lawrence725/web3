01c0bb933771fcaf551a3fe555944888e0d006a6
Lawrence <lawrence.0725@hotmail.com>
2023-03-23 14:17:07 +0800

    Update Menu cart

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,6 @@ function popCart() {
     document.getElementById("pop__cart").style.display = "flex";
     document.body.style.overflow = "hidden"; }
 
+// 訂單備註
 function textarea(e) {
-    e.style.height = "5px";
-    e.style.height = (e.scrollHeight)+"px";
-}
+    e.style = "min-height: " + (e.scrollHeight) + "px"; }
